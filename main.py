8b6e962c837667c691937e31ec2e137a8176b123
Damaris224 <mwangi204wangari@gmail.com>
2026-02-26 19:23:19 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-from database import get_products,fetch_sales
+from flask import Flask,render_template,request,redirect,url_for
+from database import get_products,fetch_sales, insert_stock
 
 #Flask instance
 app = Flask(__name__)
@@ -15,6 +15,18 @@ def products():
     products = get_products()
     return render_template('products.html',products =products)
 
+@app.route('/add_products', methods=['GET','POST'])
+def add_products():
+    if request.method == 'POST':
+        product_name = request.form['name']
+        buying_price = request.form['buying_price']
+        selling_price = request.form['selling_price']
+        new_product = (product_name,buying_price,selling_price)
+        insert_products(new_product)
+        print ("Product added successfully")
+        return redirect(url_for('products'))
+
+
 
 
 @app.route('/sales')
@@ -23,13 +35,27 @@ def sales():
     return render_template('sales.html',sales = sales)
 
 
+@app.route('/sales', methods=['GET', 'POST'])
+def sales():
+    if request.method == 'POST':
+        pid = request.form['pid']
+        quantity = request.form['quantity']
+        insert_sale((pid, quantity))   # use your database function
+        return redirect(url_for('sales'))  # reload page to show updated table
 
+   
 @app.route('/stock')
 def stock():
     value = 789
     numbers = [1,2,3,4,5,6,7,8,9]
     return render_template('stock.html',x = value,y=numbers)
 
+def stock(): 
+    if request.method == 'POST': 
+    pid = request.form['pid'] 
+    stock_quantity = request.form['stock_quantity'] 
+    insert_stock((pid, stock_quantity)) 
+    return redirect(url_for('stock'))
 
 @app.route('/dashboard')
 def dashboard():
